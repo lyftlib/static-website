1ab23debfdcee0ddd3c8e1e822181fe14acbb8b0
Matisse <mb.workadress@gmail.com>
2018-10-19 17:19:12 +0200

    basic changes for aws s3

--- src/github.js
+++
@@ -1,9 +0,0 @@
-const Github = require('github-api');
-
-// Creates a new instance of the Github class with the credentials of the user
-const api = new Github({
-    username: process.env.LYFTPV_GITHUB_USERNAME,
-    token: process.env.LYFTPV_GITHUB_TOKEN
-});
-
-module.exports = api;
