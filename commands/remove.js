b693b3a95b921901d9d2fdaa00c6ecd3297cbc5f
Matisse <mb.workadress@gmail.com>
2018-10-19 17:54:30 +0200

    Remove done

--- commands/remove.js
+++ commands/remove.js
@@ -1,17 +1,27 @@
-const { withError, getRepoName, getStatePath } = require('../src/utils');
+const { withError, getRepoName, getStatePath } = require("../src/utils");
 
 // Exports the remove command for the Github module
 module.exports = {
-    method(name) {
-        const api = require('../src/');
+  method(p_name) {
+    const api = require("../src/");
 
-        // Gets the repository and deletes it.
-        // api.getRepo(process.env.LYFTPV_GITHUB_USERNAME, getRepoName(getStatePath(name))).deleteRepo()
-        // .catch(() => withError('The repository does not exist or is already deleted'))
-        // .then(() => {
-        //     console.log(`Repository deleted`);
-        // });
-        console.log(name);
-        
-    }
+    // Gets the repository and deletes it.
+    // api.getRepo(process.env.LYFTPV_GITHUB_USERNAME, getRepoName(getStatePath(name))).deleteRepo()
+    // .catch(() => withError('The repository does not exist or is already deleted'))
+    // .then(() => {
+    //     console.log(`Repository deleted`);
+    // });
+    aws.config.loadFromPath(
+      require("path").join(__dirname, "../aws-config.json")
+    );
+    var s3 = new aws.S3();
+
+    var params = {
+        Bucket: p_name
+      };
+      s3.deleteBucket(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else     console.log(data);           // successful response
+      });
+  }
 };
